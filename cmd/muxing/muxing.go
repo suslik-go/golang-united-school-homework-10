66c281b743d9645092d1798a00ebd4964f70ae96
suslik <hasmik_margaryan@epam.com>
2022-07-12 13:52:41 -0700

    Initial commit

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -21,9 +22,39 @@ main function reads host/port from env just for an example, flavor it following 
 func Start(host string, port int) {
 	router := mux.NewRouter()
 
+	router.HandleFunc("/name/{name}", NameHandler).Methods(http.MethodGet)
+	router.HandleFunc("/bad", BadHandler).Methods(http.MethodGet)
+	router.HandleFunc("/data/{body}/{param}", DataHandler).Methods(http.MethodPost)
+	router.HandleFunc("/headers/{headers}", HeadersHandler).Methods(http.MethodPost)
 	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
 	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
 		log.Fatal(err)
+	}
+
+}
+
+func NameHandler(w http.ResponseWriter, r *http.Request) {
+	//w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	fmt.Fprint(w, "Hello, "+params["name"]+"!")
+}
+
+func BadHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+}
+
+func DataHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	w.Write([]byte(params["body"] + ":\n" + params["param"]))
+}
+
+func HeadersHandler(w http.ResponseWriter, r *http.Request) {
+	var postHeaders map[string]string
+	params := mux.Vars(r)
+	json.Unmarshal([]byte(params["headers"]), &postHeaders)
+	for key, value := range postHeaders {
+		w.Header().Set(key, value)
 	}
 }
 
